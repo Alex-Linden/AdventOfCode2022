a69a1fc085fb7577f8e72d3ac9ab17ebb0c714f0
Alex Linden <alextl2004@gmail.com>
2022-12-22 18:26:45 -0800

    building class wip

--- day15.py
+++ day15.py
@@ -2,3 +2,17 @@ data = open('day15test.txt').read().split('\n')
 # data = open('day15.txt').read().split('\n')
 
 print(data)
+
+
+for i in range(len(data)):
+    data[i] = data[i].replace("Sensor at ", "")
+    data[i] = data[i].replace(": closest beacon is at", ",")
+
+print(data)
+
+
+
+class BeaconZone:
+    def __init__(self, scan) -> None:
+        self.grid = self.make_grid()
+        self.scan = scan
